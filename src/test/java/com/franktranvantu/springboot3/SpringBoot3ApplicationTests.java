8b57f9d7926869f1faa244b8ba2b4fa0b9dd559e
Tú Văn Trần <tu.van.tran@mgm-tp.com>
2024-05-02 00:24:41 +0700

    Updated

--- src/test/java/com/franktranvantu/springboot3/SpringBoot3ApplicationTests.java
+++ src/test/java/com/franktranvantu/springboot3/SpringBoot3ApplicationTests.java
@@ -2,8 +2,10 @@ package com.franktranvantu.springboot3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/unit-test.properties")
 class SpringBoot3ApplicationTests {
 
     @Test
